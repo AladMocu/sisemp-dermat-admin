f636237b5079feda5971944a4a1145cc35ee3056
AladMocu <aa.molano@uniandes.edu.co>
2021-06-02 13:22:57 -0500

    base repo creado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sisemp-admin';
+  tableRows: any[] | undefined;
+
+  ngOnInit(): void {
+    this.tableRows = [
+      {
+        column1: 'user.name@email.com',
+        column2: 'LongText LongText LongText LongText LongText LongText LongText LongText LongText LongText LongText',
+        column3: 'Row 2',
+        date: '09-07-18',
+        type: 'search'
+      },
+      {
+        column1: 'user.name@email.com',
+        column2: 'Row 2',
+        column3: 'Wrapped Long Text Wrapped Long Text Wrapped Long Text Wrapped Long Text Wrapped Long Text Wrapped Long',
+        date: '09-08-18',
+        type: 'cart'
+      },
+      {
+        column1: 'user.name@email.com',
+        column2: 'Row 3',
+        column3: 'Row 3',
+        date: '02-14-18',
+        type: 'calendar'
+      },
+      {
+        column1: 'user.name@email.com',
+        column2: 'Row 4',
+        column3: 'Row 4',
+        date: '12-30-17',
+        type: 'search'
+      },
+      {
+        column1: 'user.name@email.com',
+        column2: 'Row 5',
+        column3: 'Row 5',
+        date: '11-12-18',
+        type: 'search'
+      }
+    ];
+  }
 }
