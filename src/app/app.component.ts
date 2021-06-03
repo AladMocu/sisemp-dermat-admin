3fd953fde0bf02671929a26dce9d8e25a5a00552
AladMocu <aa.molano@uniandes.edu.co>
2021-06-02 21:43:05 -0500

    waza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit  } from '@angular/core';
+import {Component, OnInit, TemplateRef} from '@angular/core';
+import {DialogService} from '@fundamental-ngx/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,45 +10,143 @@ import { Component, OnInit  } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'sisemp-admin';
-  tableRows: any[] | undefined;
+
+  // @ts-ignore
+  displayedRows: any[];
+
+  // @ts-ignore
+  appointments: any[];
+  searchTerm = '';
+  // @ts-ignore
+  confirmationReason: string;
+  // @ts-ignore
+  myForm: FormGroup ;
+  loading = false;
+  past = false;
+  // @ts-ignore
+  today: Date;
+
+  // tslint:disable-next-line:variable-name
+  constructor(private _dialogService: DialogService, private _fb: FormBuilder, private _ds: DataService) {}
 
   ngOnInit(): void {
-    this.tableRows = [
-      {
-        column1: 'user.name@email.com',
-        column2: 'LongText LongText LongText LongText LongText LongText LongText LongText LongText LongText LongText',
-        column3: 'Row 2',
-        date: '09-07-18',
-        type: 'search'
-      },
-      {
-        column1: 'user.name@email.com',
-        column2: 'Row 2',
-        column3: 'Wrapped Long Text Wrapped Long Text Wrapped Long Text Wrapped Long Text Wrapped Long Text Wrapped Long',
-        date: '09-08-18',
-        type: 'cart'
-      },
-      {
-        column1: 'user.name@email.com',
-        column2: 'Row 3',
-        column3: 'Row 3',
-        date: '02-14-18',
-        type: 'calendar'
-      },
-      {
-        column1: 'user.name@email.com',
-        column2: 'Row 4',
-        column3: 'Row 4',
-        date: '12-30-17',
-        type: 'search'
-      },
-      {
-        column1: 'user.name@email.com',
-        column2: 'Row 5',
-        column3: 'Row 5',
-        date: '11-12-18',
-        type: 'search'
-      }
-    ];
+    this.past = true;
+    this.today = new Date();
+    this.appointments = [];
+    this.displayedRows = this.appointments;
+
+    this.myForm = this._fb.group({
+      startInput: new FormControl(''),
+      endInput: new FormControl('')
+    });
+    this.loading = true;
+    // @ts-ignore
+    this._ds.getAllApointments().subscribe((data: any[]) => {
+      this.loading = false;
+      this.appointments = data;
+      this.cleanData();
+      this.resetSearch();
+    });
   }
+
+  searchInputChanged(event: string): void {
+    // @ts-ignore
+    const filterRows = (row): boolean => {
+      const keys = Object.keys(row);
+      return !!keys.find(key => {
+        if (row[key] != null){
+         try {
+           return row[key].toLowerCase().includes(event.toLowerCase());
+         }catch (e) {
+           return null;
+         }
+        }
+        return null;
+      });
+    };
+    if (event) {
+      // @ts-ignore
+      this.displayedRows = this.appointments
+
+
+        .filter(row => filterRows(row));
+    } else {
+      this.displayedRows = this.appointments;
+    }
+  }
+
+  cleanData(): void{
+    this.appointments.forEach((value, index) => {
+      this.appointments[index].FechayHora = new Date(value.FechayHora);
+      this.appointments[index].FinCita = new Date(value.FinCita);
+      this.appointments[index].cancelable = this.appointments[index].Estado === 'PENDIENTE';
+      this.appointments[index].past = this.appointments[index].FechayHora < this.today;
+    }, this.appointments);
+    this.appointments.sort((a, b) => a.FechayHora - b.FechayHora);
+  }
+
+  obfuscatePastData(): void{
+    if ( this.past ){
+      this.displayedRows = this.displayedRows.filter(row => !row.past);
+    }
+    else {
+      this.resetSearch();
+    }
+  }
+
+  resetSearch(): void {
+    this.displayedRows = this.appointments;
+    this.searchTerm = '';
+  }
+
+  openDialog(dialog: TemplateRef<any>): void {
+    const dialogRef = this._dialogService.open(dialog, { responsivePadding: true });
+
+    dialogRef.afterClosed.subscribe(
+      (result) => {
+        this.confirmationReason = 'Dialog closed with result: ' + result;
+        // @ts-ignore
+        const tempCita = {
+          // @ts-ignore
+          FechayHora: this.myForm.get('startInput').value.toString(),
+          // @ts-ignore
+          FinCita: this.myForm.get('endInput').value.toString(),
+        };
+        console.log(tempCita);
+        this._ds.addAppointment(tempCita).subscribe(el => console.log(el));
+        // @ts-ignore
+        this.myForm.setValue({startInput: '', endInput: ''});
+        // @ts-ignore
+        this._ds.getAllApointments().subscribe((data: any[]) => {
+          this.loading = false;
+          this.appointments = data;
+          this.cleanData();
+          this.resetSearch();
+        });
+      },
+      (error) => {
+        this.confirmationReason = 'Dialog dismissed with result: ' + error;
+      }
+    );
+  }
+  // tslint:disable-next-line:typedef
+  refresh(){
+    this.loading = true;
+    // @ts-ignore
+    this._ds.getAllApointments().subscribe((data: any[]) => {
+      this.past = true;
+      this.loading = false;
+      this.appointments = data;
+      this.cleanData();
+      this.resetSearch();
+    });
+  }
+
+
+  cancelAppointment(id: any): void{
+    console.log(id);
+    console.log(new Date(id));
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FundamentalNgxCoreModule} from '@fundamental-ngx/core'
+import { FundamentalNgxCoreModule, FdDatetimeModule} from '@fundamental-ngx/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ScheduleComponent } from './schedule/schedule.component';
+import {SchedulerModule} from '@progress/kendo-angular-scheduler';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FundamentalNgxCoreModule
+    FundamentalNgxCoreModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FdDatetimeModule,
+    SchedulerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
